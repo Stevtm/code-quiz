199420e66fcd13101e3507e7535bdbb8984ccf06
Stevtm <steventmaddison@outlook.com>
2021-03-14 23:12:34 -0400

    add function that pushes quiz answers to DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,26 +7,47 @@ var choicesEl = document.querySelector("#choices");
 var questions = [
 	{
 		question: "Arrays in JavaScript can be used to store which data types?",
-		1: "Strings",
-		2: "Numbers",
-		3: "Booleans",
-		4: "All of the above",
+		a: "Strings",
+		b: "Numbers",
+		c: "Booleans",
+		d: "All of the above",
 		correct: 4,
 	},
 	{
 		question: "Which of the following is not a data type in JavaScript?",
-		1: "string",
-		2: "object",
-		3: "int",
-		4: "number",
+		a: "string",
+		b: "object",
+		c: "int",
+		d: "number",
 		correct: 3,
 	},
-	{
-		question: "",
-		a1: "Strings",
-		a2: "Numbers",
-		a3: "Booleans",
-		a4: "All of the above",
-		correct: "a4",
-	},
 ];
+
+console.log(questions);
+
+// ----- functions that manipulate the DOM -----
+
+// function that creates a list element for each quiz answer option
+var createQuestionEl = function (questionObj) {
+	// --- perform for each of the 4 options ---
+	// create array of only the options
+	var options = [questionObj.a, questionObj.b, questionObj.c, questionObj.d];
+
+	// create a list item for each of the 4 options
+	for (var i = 0; i < options.length; i++) {
+		// create list item
+		var listItemEl = document.createElement("li");
+		listItemEl.className = "answer-option";
+
+		// add the answer id (the array index + 1) as a custom attribute
+		listItemEl.setAttribute("answer-id", i + 1);
+
+		// add the question content
+		listItemEl.textContent = options[i];
+
+		// append the list item to the unordered list
+		choicesEl.appendChild(listItemEl);
+	}
+};
+
+createQuestionEl(questions[0]);
