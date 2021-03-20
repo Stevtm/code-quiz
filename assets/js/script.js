4f405ce4db7e0b32943c3c543bc097637d82612f
Stevtm <steventmaddison@outlook.com>
2021-03-19 21:51:24 -0400

    refactor js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// ----- page set up -----
+// ----- script set up -----
 // create references to relevant HTML ids
 var headerEl = document.querySelector("header");
 var titleEl = document.querySelector("#title");
@@ -19,6 +19,12 @@ if (!highScores) {
 
 // declare time variable starting at 45 seconds
 var timeRemaining = 45;
+
+// declare counter that tracks the user score (correct answer: +1 score)
+var score = 0;
+
+// declare counter that tracks the current position in the questions array
+var questionCounter = 0;
 
 // ----- create array of randomized questions for quiz -----
 // declare array with questions & answers (unshuffled)
@@ -113,30 +119,23 @@ questions = questions.sort(function () {
 	return Math.random() - 0.5;
 });
 
-// create a counter that tracks the current position in the questions array
-var questionCounter = 0;
+// ----- functions that execute when the quiz starts -----
 
-// create a counter that tracks the user score (correct answer: +1 score)
-var score = 0;
-
-// ----- functions that manipulate the DOM -----
-
-// function that displays the first question and replaces the existing screen text
+// function that displays the first question and replaces the opening screen text
 var createQuestionEl = function (questionObj) {
-	// --- perform for each of the 4 options ---
 	// create array of only the options
 	var options = [questionObj.a, questionObj.b, questionObj.c, questionObj.d];
 
-	// create a list item for each of the 4 options
+	// --- create list item for each of the answer options ---
 	for (var i = 0; i < options.length; i++) {
-		// create list item
+		// create list item and apply bootstrap styling
 		var listItemEl = document.createElement("li");
 		listItemEl.className = "btn btn-secondary";
 
-		// add the answer id (the array index + 1) as a custom attribute
+		// add answer id (the array index + 1) as a custom attribute
 		listItemEl.setAttribute("answer-id", i + 1);
 
-		// add the question content
+		// add question content
 		listItemEl.textContent = options[i];
 
 		// append the list item to the unordered list
@@ -151,14 +150,50 @@ var createQuestionEl = function (questionObj) {
 	questionEl.textContent = questionObj.question;
 };
 
+// timer function that counts down 45 seconds
+var countdown = function () {
+	var timeInterval = setInterval(function () {
+		// check if the quiz has already been ended by all questions being answered
+		if (questionCounter < questions.length) {
+			// check if the quiz time has expired
+			if (timeRemaining > 1) {
+				// if the time has not expired, decrement the counter and update the DOM
+				timeRemaining--;
+				timeEl.textContent = timeRemaining + "s";
+			} else {
+				// if the time has expired, clear the interval and end the game
+				alert("You ran out of time!");
+				clearInterval(timeInterval);
+				timeEl.textContent = "0s";
+				endGame();
+			}
+		} else {
+			// if the quiz has been ended by all questions being entered, the endGame function will be called by the choicesEl event listener. Clear the interval.
+			clearInterval(timeInterval);
+			timeEl.textContent = "0s";
+		}
+	}, 1000);
+};
+
+// click listener for quiz start
+startEl.addEventListener("click", function () {
+	// start the timer
+	countdown();
+
+	// display the first quiz question
+	createQuestionEl(questions[0]);
+});
+
+// ----- functions that execute when the game is being played -----
+
 // function that compares the clicked response to the correct answer
 var checkCorrect = function (id, questionObj) {
-	console.log(questionObj.correct);
 	if (id === questionObj.correct) {
 		// increase score by 1 for the correct answer
 		score++;
 		alert("You've picked the correct response!");
 	} else {
+		// decrese time remaining by 10s for wrong answer
 		timeRemaining -= 10;
 		alert("That's the wrong answer!");
 	}
@@ -182,33 +217,41 @@ var replaceQuestion = function (questionObj) {
 	d.textContent = questionObj.d;
 };
 
-// timer function that counts down 1 minute
-var countdown = function () {
-	var timeInterval = setInterval(function () {
-		// check if the quiz has already been ended by all questions being answered
-		if (questionCounter < questions.length) {
-			// check if the quiz time has expired
-			if (timeRemaining > 1) {
-				timeRemaining--;
-				timeEl.textContent = timeRemaining + "s";
-			} else {
-				clearInterval(timeInterval);
-				timeEl.textContent = "0s";
-				endGame();
-			}
-		} else {
-			clearInterval(timeInterval);
-			timeEl.textContent = "0s";
-		}
-	}, 1000);
-};
+// ----- click to listen for quiz responses -----
+choicesEl.addEventListener("click", function (event) {
+	var target = event.target;
+
+	// if the click was not on one of the <li> elements, return from the function
+	if (target.getAttribute("answer-id") === null) {
+		return;
+	}
+
+	// get the user response to a question and save as int
+	var id = parseInt(event.target.getAttribute("answer-id"));
+
+	// check that the response is correct with the checkCorrect function
+	checkCorrect(id, questions[questionCounter]);
+
+	// increase the question counter to the next array iteration
+	questionCounter++;
+
+	// if there are remaining questions in the array, display the next question
+	if (questionCounter < questions.length) {
+		replaceQuestion(questions[questionCounter]);
+	} // if there are no remaining questions in the array, end the quiz
+	else {
+		endGame();
+	}
+});
+
+// ----- functions that execute when the quiz is completed
 
 // game-end function that opens the high score recording page
 var endGame = function () {
 	// alert the user that the game is over and tell them their score
 	alert(`The quiz is over! Your score was ${score}`);
 
-	// remove the header section and ul from the DOM
+	// remove the header section and <ul> from the DOM
 	headerEl.remove();
 	choicesEl.remove();
 
@@ -223,10 +266,11 @@ var endGame = function () {
 	submitEl.className = "score-btn btn btn-primary";
 	submitEl.textContent = "Submit";
 
+	// append the form and submit button to the score input div
 	scoreInputEl.appendChild(formEl);
 	scoreInputEl.appendChild(submitEl);
 
-	// add a listener for the submit button being clicked
+	// push the player name and score to localStorage when the "submit" button is pressed
 	submitEl.addEventListener("click", function () {
 		getPlayerName(formEl, submitEl);
 	});
@@ -265,6 +309,44 @@ var recordScore = function (playerName) {
 	localStorage.setItem("highScores", JSON.stringify(highScores));
 };
 
+// function that inserts a table of high scores
+var createHighScoreTable = function (topScores) {
+	// create the table header and append to table element
+	var tableHeaderEl = document.createElement("thead");
+	var tableHeaderRowEl = document.createElement("tr");
+	tableHeaderRowEl.innerHTML =
+		"<th scope='col'>#</th> <th scope='col'>Player Name</th> <th scope='col'>Score</th>";
+	tableHeaderEl.appendChild(tableHeaderRowEl);
+	highScoresEl.appendChild(tableHeaderEl);
+
+	// create table body element
+	var tableBodyEl = document.createElement("tbody");
+	// create a row for each of the highscores and display #, name & score
+	for (var i = 0; i < topScores.length; i++) {
+		// create a tr and td elements
+		var tableEl = document.createElement("tr");
+		var tableIndexEl = document.createElement("th");
+		tableIndexEl.setAttribute("scope", "row");
+		var tableNameEl = document.createElement("td");
+		var tableScoreEl = document.createElement("td");
+
+		tableIndexEl.textContent = (i + 1).toString();
+		tableNameEl.textContent = topScores[i].name;
+		tableScoreEl.textContent = topScores[i].score;
+
+		// append table elements to the table row
+		tableEl.appendChild(tableIndexEl);
+		tableEl.appendChild(tableNameEl);
+		tableEl.appendChild(tableScoreEl);
+
+		// append the table row to the table body
+		tableBodyEl.appendChild(tableEl);
+	}
+
+	// append the entire table body to the table element
+	highScoresEl.appendChild(tableBodyEl);
+};
+
 // function that shows high scores
 var showHighScores = function () {
 	// change the content of the h2 element
@@ -284,74 +366,3 @@ var showHighScores = function () {
 	// insert table onto DOM
 	createHighScoreTable(topScores);
 };
-
-// function that inserts a table of high scores
-var createHighScoreTable = function (topScores) {
-	// create the table header and append to table element
-	var tableHeaderEl = document.createElement("thead");
-	var tableHeaderRowEl = document.createElement("tr");
-	tableHeaderRowEl.innerHTML =
-		"<th scope='col'>#</th> <th scope='col'>Player Name</th> <th scope='col'>Score</th>";
-	tableHeaderEl.appendChild(tableHeaderRowEl);
-	highScoresEl.appendChild(tableHeaderEl);
-
-	// create table body element
-	var tableBodyEl = document.createElement("tbody");
-	// create a row for each of the highscores
-	for (var i = 0; i < topScores.length; i++) {
-		// create a tr and td elements
-		var tableEl = document.createElement("tr");
-		var tableIndexEl = document.createElement("th");
-		tableIndexEl.setAttribute("scope", "row");
-		var tableNameEl = document.createElement("td");
-		var tableScoreEl = document.createElement("td");
-
-		tableIndexEl.textContent = (i + 1).toString();
-		tableNameEl.textContent = topScores[i].name;
-		tableScoreEl.textContent = topScores[i].score;
-
-		tableEl.appendChild(tableIndexEl);
-		tableEl.appendChild(tableNameEl);
-		tableEl.appendChild(tableScoreEl);
-
-		tableBodyEl.appendChild(tableEl);
-	}
-
-	highScoresEl.appendChild(tableBodyEl);
-};
-
-// ----- click to initiate quiz -----
-startEl.addEventListener("click", function () {
-	// start the timer
-	countdown();
-
-	// display the first quiz question
-	createQuestionEl(questions[0]);
-});
-
-// ----- click to listen for quiz responses -----
-choicesEl.addEventListener("click", function (event) {
-	var target = event.target;
-
-	// check that the click was on one of the <li> elements
-	if (target.getAttribute("answer-id") === null) {
-		return;
-	}
-
-	// get the user response to a question and save as int
-	var id = parseInt(event.target.getAttribute("answer-id"));
-
-	// check that the response is correct with the checkCorrect function
-	checkCorrect(id, questions[questionCounter]);
-
-	// increase the question counter to the next array iteration
-	questionCounter++;
-
-	// if there are remaining questions in the array, display the next question
-	if (questionCounter < questions.length) {
-		replaceQuestion(questions[questionCounter]);
-	} // if there are no remaining questions in the array, end the quiz
-	else {
-		endGame();
-	}
-});
